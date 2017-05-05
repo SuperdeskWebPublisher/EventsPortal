6df038ebb68ff90ef0dae4a33598f4dc26c92d81
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2017-05-05 13:24:48 +0200

    Add single event api

--- src/EventsBundle/Controller/EventsController.php
+++ src/EventsBundle/Controller/EventsController.php
@@ -5,6 +5,7 @@ namespace SWP\EventsBundle\Controller;
 use SWP\Component\Common\Criteria\Criteria;
 use SWP\Component\Common\Pagination\PaginationData;
 use SWP\Component\Common\Response\ResourcesListResponse;
+use SWP\Component\Common\Response\SingleResourceResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
@@ -26,7 +27,7 @@ class EventsController extends Controller
      * )
      * @Route("/api/{version}/events/", options={"expose"=true}, defaults={"version"="v1"}, name="swp_api_events_list")
      * @Method("GET")
-     * Cache(expires="10 minutes", public=false)
+     * @Cache(expires="10 minutes", public=true)
      *
      * @return ResourcesListResponse
      */
@@ -36,5 +37,41 @@ class EventsController extends Controller
         $events = $eventRepository->getPaginatedByCriteria(new Criteria(), [], new PaginationData($request));
 
         return new ResourcesListResponse($events);
+    }
+
+    /**
+     * Get single Event details.
+     *
+     * @ApiDoc(
+     *     resource=true,
+     *     description="Get single Event details",
+     *     statusCodes={
+     *         200="Returned on success."
+     *     },
+     *     requirements={
+     *         {"name"="id", "dataType"="integer|string", "required"=true, "description"="Event `id` or `guid`"}
+     *     }
+     * )
+     * @Route("/api/{version}/events/{id}", options={"expose"=true}, defaults={"version"="v1"}, name="swp_api_events_get")
+     * @Method("GET")
+     * @Cache(expires="10 minutes", public=true)
+     *
+     * @return SingleResourceResponse
+     */
+    public function getAction($id)
+    {
+        $eventRepository = $this->get('swp.repository.event');
+        $criteria = new Criteria();
+        if (false === strpos($id, 'urn:newsml')) {
+            $criteria->set('id', $id);
+        } else {
+            $criteria->set('guid', $id);
+        }
+
+        $event = $eventRepository->getQueryByCriteria($criteria, [], 'e')
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        return new SingleResourceResponse($event);
     }
 }
